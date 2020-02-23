6205e97122698445b9687ea9deceedc686de03b1
Jonah Stegman <jstegman@uoguelph.ca>
2020-02-23 17:44:07 -0500

    added is word

--- A2/wordscram.adb
+++ A2/wordscram.adb
@@ -4,6 +4,19 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with ada.numerics.discrete_random;
 procedure wordscram is 
+
+        -- checks if input is a word
+        function isWord(word: string) return boolean is
+        begin
+            for j in word'range loop
+                if word(j) not in 'A'..'Z' and word(j) not in 'a'..'z' then 
+                    return false;
+                end if;
+            end loop;
+            return true;
+        end isWord;
+
+
     -- gets the filename from the user
     function getFilename return string is
         filename : string (1..100);
@@ -19,8 +32,7 @@ procedure wordscram is
                 open(file,in_file,filename(1..size));
                 close(file);
             exception
-                when Name_Error =>
-                    flag := 1;
+                when Name_Error => flag := 1;
             end;
             exit when flag = 0;
         end loop;
@@ -36,17 +48,47 @@ procedure wordscram is
                 name => filename);
             loop
                 declare
-                    line: string := get_line (input);
-                    put_line(line);
+                    line: string := get_line(input);
+                    word : string(1..100);
+                    boolWord : boolean;
+                    j : integer := 1;
+                    n : integer := 0;
+                    subtype lower is character range 'a'..'z';
+                    subtype upper is character range 'A'..'Z';
+                    subtype number is character range '0'..'9';
+                begin
+                    while  line'last >= j loop
+                        if line(j) in upper or line(j) in lower or line(j) in number then 
+                            n := n + 1;
+                            word(n) := line(j);
+                        else
+                            boolWord := isWord(word(1..n));
+                            if boolWord then
+                                
+                            else
+                                put(word(1..n));
+                            end if;
+                            n := 0;
+                            put(line(j));
+                        end if;
+                        j := j + 1;
+                        if line'last < j then
+                            boolWord := isWord(word(1..n));
+                            if boolWord then
+                                put(word(1..n));
+                            else
+                                put(word(1..n));
+                            end if;
+                        end if;
+                    end loop;
                 end;
+                put_line("");
             end loop;
-            close(input);
         exception
             when End_Error =>
                 if is_open(input) then 
                     close(input);
                 end if;
-            put_line(filename);
             return 0;
         end processText;
 
@@ -54,7 +96,6 @@ procedure wordscram is
     flag: integer:=1;
     userfile: constant string := getFilename;
 begin
-
     flag:=processText(userfile);
     if flag = 0 then
         put_line("success");
@@ -83,7 +124,3 @@ end wordscram;
 --    put_line(randRange'Image(num));
 --    return num;
 --end randomInt;
-
---function isWord() return boolean is
---begin
---end isWord;
