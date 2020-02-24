42ce824e95043dd80e99d041e48d15429764e13d
Jonah Stegman <jstegman@uoguelph.ca>
2020-02-23 20:16:41 -0500

    almost done

--- A2/wordscram.adb
+++ A2/wordscram.adb
@@ -4,17 +4,17 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with ada.numerics.discrete_random;
 procedure wordscram is 
-
+     type int_arr is array (1..100) of integer;
         -- checks if input is a word
-        function isWord(word: string) return boolean is
-        begin
-            for j in word'range loop
-                if word(j) not in 'A'..'Z' and word(j) not in 'a'..'z' then 
-                    return false;
-                end if;
-            end loop;
-            return true;
-        end isWord;
+    function isWord(word: string) return boolean is
+    begin
+        for j in word'range loop
+            if word(j) not in 'A'..'Z' and word(j) not in 'a'..'z' then 
+                return false;
+            end if;
+        end loop;
+        return true;
+    end isWord;
 
 
     -- gets the filename from the user
@@ -39,6 +39,58 @@ procedure wordscram is
         return filename(1..size);
     end getFilename;
 
+    --generates a random number
+    function randomInt(max:integer; arr: int_arr) return integer  is
+            type Rand_Range is new integer range 2..max;
+            package Rand_Int is new Ada.Numerics.Discrete_Random(Rand_Range);
+            seed : Rand_Int.Generator;
+            num : Rand_Range;
+            check : boolean := false;
+            i : integer := 1;
+            number : integer;
+    begin
+        loop
+            check := true;
+            Rand_Int.Reset(seed);
+            num := Rand_Int.Random(seed);
+            number := Integer'Value(Rand_range'Image(num));
+            loop
+                if number = arr(i) then
+                    check := false;
+                    exit;
+                end if;
+                i := i + 1;
+                exit when i > max;
+            end loop;
+            exit when check;
+        end loop;
+        return number;
+    end randomInt;
+
+
+    -- scrambles the word
+    function scrambleWord(original:string; max:integer) return string is
+        num : integer :=1;
+        j : integer := 2;
+        k : integer := 1;
+        word : string := original;
+        arr : int_arr := (1..100 => 0);
+    begin
+        loop
+            num := randomInt(max,arr);
+            if num = 0 then
+                exit;
+            end if;
+            arr(k) := num;
+            word(num) := original(j);
+            j := j + 1;
+            k := k + 1;
+            exit when j > max;
+        end loop;
+        return word;
+    end scrambleWord;
+
+
     -- Reads in file
     function processText(filename: string) return integer is
         input : file_type;
@@ -48,9 +100,8 @@ procedure wordscram is
                 name => filename);
             loop
                 declare
-                    line: string := get_line(input);
+                    line: constant string := get_line(input);
                     word : string(1..100);
-                    boolWord : boolean;
                     j : integer := 1;
                     n : integer := 0;
                     subtype lower is character range 'a'..'z';
@@ -62,9 +113,8 @@ procedure wordscram is
                             n := n + 1;
                             word(n) := line(j);
                         else
-                            boolWord := isWord(word(1..n));
-                            if boolWord then
-                                
+                            if isWord(word(1..n)) and n > 3 then
+                                put(scrambleWord(word(1..n),n-1));
                             else
                                 put(word(1..n));
                             end if;
@@ -73,9 +123,8 @@ procedure wordscram is
                         end if;
                         j := j + 1;
                         if line'last < j then
-                            boolWord := isWord(word(1..n));
-                            if boolWord then
-                                put(word(1..n));
+                            if isWord(word(1..n)) and n > 3 then
+                                put(scrambleWord(word(1..n),n-1));
                             else
                                 put(word(1..n));
                             end if;
@@ -101,26 +150,3 @@ begin
         put_line("success");
     end if;
 end wordscram;
-
-
---function processText()
---begin
---end processText;
-
---function scrambleWord()
---begin
---end scrambleWord;
-
---function randomInt() return integer is
---    type randRange is new Integer range 1..100;
---    package Rand_Int is new ada.numerics.discrete_random(randRange);
---    use Rand_Int;
---    gen : Generator;
---    num : randRange
---    num: integer
---begin
---    reset(gen);
---    num:= random(gen);
---    put_line(randRange'Image(num));
---    return num;
---end randomInt;
